f84eb16db96a70938807de45836e783042063f80
Leon Jungemeyer <leon.jungemeyer@me.com>
2020-06-21 21:50:29 +0200

    Bug fixes

--- fit_faces.py
+++ fit_faces.py
@@ -6,6 +6,7 @@ import bz2
 from keras.utils import get_file
 from ffhq_dataset.landmarks_detector import LandmarksDetector
 from shutil import copyfile
+import tqdm
 
 LANDMARKS_MODEL_URL = 'http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2'
 
@@ -30,8 +31,8 @@ def main():
 
     args, other_args = parser.parse_known_args()
 
-    #landmarks_model_path = unpack_bz2(get_file('shape_predictor_68_face_landmarks.dat.bz2', LANDMARKS_MODEL_URL, cache_subdir='temp'))
-    landmarks_model_path = unpack_bz2('models/shape_predictor_68_face_landmarks.dat.bz2')
+    landmarks_model_path = unpack_bz2(get_file('shape_predictor_68_face_landmarks.dat.bz2', LANDMARKS_MODEL_URL, cache_subdir='temp'))
+    #landmarks_model_path = unpack_bz2('models/shape_predictor_68_face_landmarks.dat.bz2')
     landmarks_detector = LandmarksDetector(landmarks_model_path)
 
     #Copy file to new location to work on it
@@ -43,6 +44,7 @@ def main():
             filename = os.path.splitext(f)[0]
             for i, generated_face_landmarks in enumerate(landmarks_detector.get_landmarks(args.face_path + filename + ".png"), start=1):
                 face_replace(args.dst_file, args.face_path  + filename + ".png", args.mask_path + filename + ".png", args.face_landmarks_path + filename + ".npy", generated_face_landmarks, args.dst_file )
-
+    print("Done!")
+    
 if __name__ == "__main__":
     main()
